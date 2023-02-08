13a919e00cad6447215876e52aaa70af607a2088
fljonllc <121655258+fljonllc@users.noreply.github.com>
2023-02-08 12:46:47 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,9 +9,6 @@ terraform {
  Configure the AWS Provider
  provider "aws" {
   region     = "us-east-1" 
-#new line added here
-access_key = “AKIA3PEJPIK77ZTYGVWN”
-secret_key = “XGWhHCJSmmXKWMC6MHQe2CVXie5XWMdUSUdFphqE”
 }
 
 # provider "aws" {

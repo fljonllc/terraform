98aa1c1b836399c84179765b7a75e7a17673f0a5
fljonllc <121655258+fljonllc@users.noreply.github.com>
2023-02-08 13:02:40 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "4.53.0"
     }
   }
 }
-# provider "aws" {
-#  region                   = "us-east-1"
-#  shared_credentials_files = ["~/.aws/credentials"]
-#  profile                  = "vscode"
-#}
+
+provider "aws" {
+  # Configuration options
+}

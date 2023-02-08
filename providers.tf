60d33eeb5cac3e728dc06768ffd4e0b57b896621
fljonllc <121655258+fljonllc@users.noreply.github.com>
2023-02-08 13:29:55 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 
 provider "aws" {
   region = "us-eat-1
-  access_key = AKIA3PEJPIK77ZTYGVWN
-  secret_key = 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
+  access_key = "AKIA3PEJPIK77ZTYGVWN"
+  secret_key = "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"
   # Configuration options
 }

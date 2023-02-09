d87e68593afdac780b53ec671bf54847b4f81140
fljonllc <121655258+fljonllc@users.noreply.github.com>
2023-02-08 22:45:15 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,11 +9,5 @@ terraform {
 provider "aws" {
   region = "us-eat-1"
   profile = "default"
-  withCredentials([[
-                    $class: 'AmazonWebServicesCredentialsBinding',
-                    credentialsId: 'Jenkins-Aws-Cred',
-                    accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                    secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-                ]])
-  # Configuration options
+  
 }

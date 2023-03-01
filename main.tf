236505bee1f535883068b2d475e53ae58cd05e57
fljonllc <121655258+fljonllc@users.noreply.github.com>
2023-02-28 22:15:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 # Configure the AWS VPC
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_vpc" "ntc_vpc" {
   cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true

3920f767c6e65d91575a5955e545fe5508d0b77b
fljonllc <121655258+fljonllc@users.noreply.github.com>
2023-02-28 21:55:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
 # Configure the AWS VPC
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_vpc" "ntc_vpc" {
   cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true

ac77ec340cbf82579a6bfb550c7c9be9baf70bbe
fljonllc <121655258+fljonllc@users.noreply.github.com>
2023-02-28 21:06:03 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,10 +2,3 @@ variable "host_os" {
   type    = string
   default = "linux"
 }
-
-variable "access_key" {
-  description = "My AWS access key"
-}
-variable "secret_key" {
-  description = "My AWS secret key"
-}
